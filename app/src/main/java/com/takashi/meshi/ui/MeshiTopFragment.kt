4855b621b73a71b5776639fb82078fefd5a98ca5
tks10 <tktktks10@gmail.com>
2018-09-04 13:28:24 +0900

    clean

--- app/src/main/java/com/takashi/meshi/ui/MeshiTopFragment.kt
+++ app/src/main/java/com/takashi/meshi/ui/MeshiTopFragment.kt
@@ -1,32 +1,12 @@
 package com.takashi.meshi.ui
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
 import android.os.Bundle
-import android.os.ParcelFileDescriptor
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.dena2018.protein.onetimechat.R
-import com.dena2018.protein.onetimechat.api.Api
-import com.dena2018.protein.onetimechat.model.Token
-import com.dena2018.protein.onetimechat.util.ApiErrorHandler
-import com.dena2018.protein.onetimechat.util.ImageConverter
-import com.dena2018.protein.onetimechat.util.UUIDManager
 import com.takashi.meshi.R
 import com.takashi.meshi.util.UUIDManager
-import kotlinx.android.synthetic.main.sign_up_fragment.*
-import kotlinx.android.synthetic.main.sign_up_fragment.view.*
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
-import java.io.FileDescriptor
-import java.io.IOException
 
 
 class SignUpFragment : Fragment() {
